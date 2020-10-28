40e111ad1f732ffd201f64175f0632b13cc8e587
SadhuArthik <saiarthik424@gmail.com>
2020-10-28 12:00:01 +0530

    First Commit

---
+++ src/main/java/com/example/demo/MyFinalappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinalappApplication.class, args);
+	}
+
+}
